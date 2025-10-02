bb742e76db4bb51a6b5001bd7f636160dc5e6b09
ctrlaltcrew <ctrlaltcreww@gmail.com>
2025-10-02 23:25:21 +0500

    your message

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -67,7 +67,7 @@ const Header = () => {
                 <img
                   src={Logo}
                   alt="CERA Medical Logo"
-                  className="h-12 w-auto"
+                  className="h-13 w-auto"
                 />
               </Link>
             </div>
