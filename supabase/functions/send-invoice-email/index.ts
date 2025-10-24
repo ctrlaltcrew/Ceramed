d7f2d846e7ffe49ebc3e0312b4b32754cf26b2e0
ctrlaltcrew <ctrlaltcreww@gmail.com>
2025-10-24 23:42:11 +0500

    g

--- supabase/functions/send-invoice-email/index.ts
+++ supabase/functions/send-invoice-email/index.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   "https://xpaqoturecevoyjjmwez.supabase.co",
-  "<YOUR_PUBLIC_ANON_KEY>"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwYXFvdHVyZWNldm95amptd2V6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzOTYzODIsImV4cCI6MjA3Mzk3MjM4Mn0.ohoFcJIiYeeZ3b16o_8U5OeKXgPez3JTMAD7maAtT7c"
 );
 
 Deno.serve(async (req) => {
@@ -31,11 +31,6 @@ Deno.serve(async (req) => {
       <p><strong>Total:</strong> ₨${total}</p>
       <p>We hope to serve you again soon!</p>
     `;
-    const supabase = createClient(
-  "https://xpaqoturecevoyjjmwez.supabase.co",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
-
 
     const { data, error } = await resend.emails.send({
       from: "Cera Biomed Vision <support@ceramed.org>",
